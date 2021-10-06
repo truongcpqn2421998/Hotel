5b3e8514f978b0bc13561d0c2505e8ebea83c40d
truongcpqn2421998 <truongcpqn2421998@gmail.com>
2021-10-06 14:52:02 +0700

    update

--- src/manager/ManagerHotel.java
+++ src/manager/ManagerHotel.java
@@ -5,7 +5,9 @@ import model.Customer;
 import model.Room;
 import model.Order;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ManagerHotel {
     ArrayList<Customer> customerArrayList=new ArrayList<>();
@@ -56,5 +58,88 @@ public class ManagerHotel {
         }
         return total;
     }
+    public void checkIdToGetMoney(ArrayList<Order> orders) {
+        Scanner str=new Scanner(System.in);
+        String check=null;
+        long totalPrice=0;
+        do {
+            System.out.println("Enter the ID to find the amount to be paid by the customer:");
+            String findID=str.nextLine();
+            for (int i = 0; i < orders.size(); i++) {
+                if (orders.get(i).getCustomer().getID().equals(findID)) {
+                    check=orders.get(i).getCustomer().getName();
+                    totalPrice+=orders.get(i).getTotalPrice();
+                }
+            }
+            if(check!=null){
+                System.out.println(check+" must paid "+totalPrice);
+            }
+            if(check==null){
+                System.out.println("Not found ID");
+            }
+        }while (check==null);
+    }
+    public void displayOrder(ArrayList<Order> orders) {
+        for (int i = 0; i < orders.size(); i++) {
+            System.out.println("Customer"+(i+1));
+            System.out.println(
+                    orders.get(i).getCustomer().toString()
+            );
+            System.out.println(
+                    orders.get(i).getRoom().toString()
+            );
+        }
+    }
+    public void addOrder(ArrayList<Customer> customerArrayList, ArrayList<Room> roomArrayList, ArrayList<Order> orders) {
+        Scanner num=new Scanner(System.in);
+        String customer=null;
+        do {
+            System.out.println("Enter Customer' name want to Order:");
+            String name = num.nextLine();
+            for (int i = 0; i < customerArrayList.size(); i++) {
+                if (customerArrayList.get(i).getName().equals(name)) {
+                    customer = customerArrayList.get(i).getName();
+                    int indexRoom=findRoom(roomArrayList);
+                    System.out.println("Enter checkIn day:");
+                    int dayIn = num.nextInt();
+                    System.out.println("Enter checkIn month:");
+                    int monthIn = num.nextInt();
+                    System.out.println("Enter checkIn year:");
+                    int yearIn = num.nextInt();
+                    System.out.println("Enter checkOut day");
+                    int dayOut = num.nextInt();
+                    System.out.println("Enter checkOut month");
+                    int monthOut = num.nextInt();
+                    System.out.println("Enter checkOut year");
+                    int yearOut = num.nextInt();
+                    LocalDate in=LocalDate.of(yearIn,monthIn,dayIn);
+                    LocalDate out=LocalDate.of(yearOut,monthOut,dayOut);
+                    orders.add(new Order(in, out, customerArrayList.get(i), roomArrayList.get(indexRoom)));
+                }
+            }
+            if(customer==null){
+                System.out.println("This customer does not exist");
+            }
+        }while (customer==null);
+    }
+    public int findRoom( ArrayList<Room> roomArrayList) {
+        Scanner str=new Scanner(System.in);
+        int index=-1;
+        do{
+            System.out.println("Enter Room's type:");
+            String roomType = str.nextLine();
+            for (int i = 0; i < roomArrayList.size(); i++) {
+                if (roomArrayList.get(i).getType().equals(roomType)){
+                    index=i;
+                }
+            }
+            if (index==-1) {
+                System.out.println("This room type is not available in the hotel");
+            }
+        }while (index==-1);
+        return index;
+    }
+
+
 
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -44,97 +44,15 @@ public class Main {
         int numOrder=num.nextInt();
         for (int i = 0; i < numOrder; i++) {
             System.out.println(i+1+":");
-            addOrder( customerArrayList, roomArrayList, orders);
+            hotel.addOrder( customerArrayList, roomArrayList, orders);
         }
-        displayOrder(orders);
+        hotel.displayOrder(orders);
         long allTotal=hotel.getAllTotal();
         System.out.println("Total Price:"+allTotal);
 
-        checkIdToGetMoney(orders);
-    }
-    private static void checkIdToGetMoney(ArrayList<Order> orders) {
-        Scanner str=new Scanner(System.in);
-        String check=null;
-        long totalPrice=0;
-        do {
-            System.out.println("Enter the ID to find the amount to be paid by the customer:");
-            String findID=str.nextLine();
-            for (int i = 0; i < orders.size(); i++) {
-                if (orders.get(i).getCustomer().getID().equals(findID)) {
-                    check=orders.get(i).getCustomer().getName();
-                    totalPrice+=orders.get(i).getTotalPrice();
-                }
-            }
-            if(check!=null){
-                System.out.println(check+" must paid "+totalPrice);
-            }
-            if(check==null){
-                System.out.println("Not found ID");
-            }
-        }while (check==null);
+        hotel.checkIdToGetMoney(orders);
     }
 
-    private static void displayOrder(ArrayList<Order> orders) {
-        for (int i = 0; i < orders.size(); i++) {
-            System.out.println("Customer"+(i+1));
-            System.out.println(
-                    orders.get(i).getCustomer().toString()
-            );
-            System.out.println(
-                    orders.get(i).getRoom().toString()
-            );
-        }
-    }
 
-    private static void addOrder(ArrayList<Customer> customerArrayList, ArrayList<Room> roomArrayList, ArrayList<Order> orders) {
-        Scanner num=new Scanner(System.in);
-        String customer=null;
-        do {
-            System.out.println("Enter Customer' name want to Order:");
-            String name = num.nextLine();
-            for (int i = 0; i < customerArrayList.size(); i++) {
-                if (customerArrayList.get(i).getName().equals(name)) {
-                    customer = customerArrayList.get(i).getName();
-                    int indexRoom=findRoom(roomArrayList);
-                    System.out.println("Enter checkIn day:");
-                    int dayIn = num.nextInt();
-                    System.out.println("Enter checkIn month:");
-                    int monthIn = num.nextInt();
-                    System.out.println("Enter checkIn year:");
-                    int yearIn = num.nextInt();
-                    System.out.println("Enter checkOut day");
-                    int dayOut = num.nextInt();
-                    System.out.println("Enter checkOut month");
-                    int monthOut = num.nextInt();
-                    System.out.println("Enter checkOut year");
-                    int yearOut = num.nextInt();
-                    LocalDate in=LocalDate.of(yearIn,monthIn,dayIn);
-                    LocalDate out=LocalDate.of(yearOut,monthOut,dayOut);
-                    orders.add(new Order(in, out, customerArrayList.get(i), roomArrayList.get(indexRoom)));
-                }
-            }
-            if(customer==null){
-                System.out.println("This customer does not exist");
-            }
-        }while (customer==null);
-    }
-
-    private static int findRoom( ArrayList<Room> roomArrayList) {
-        Scanner str=new Scanner(System.in);
-        int index=-1;
-        do{
-            System.out.println("Enter Room's type:");
-            String roomType = str.nextLine();
-            for (int i = 0; i < roomArrayList.size(); i++) {
-                if (roomArrayList.get(i).getType().equals(roomType)){
-                index=i;
-                }
-            }
-            if (index==-1) {
-                System.out.println("This room type is not available in the hotel");
-            }
-        }while (index==-1);
-        return index;
-    }
 }
 
